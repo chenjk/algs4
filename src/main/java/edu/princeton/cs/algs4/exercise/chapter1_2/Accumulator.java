71b016033437befd6c0f334012e402acd55c3559
chenjk <18317333@qq.com>
2018-09-29 00:30:19 +0800

    init

---
+++ src/main/java/edu/princeton/cs/algs4/exercise/chapter1_2/Accumulator.java
@@ -0,0 +1,20 @@
+package edu.princeton.cs.algs4.exercise.chapter1_2;
+
+public class Accumulator {
+    private double total;
+    private int N;
+
+    public void addDataValue(double val) {
+        N++;
+        total += val;
+    }
+
+    public double mean() {
+        return total / N;
+    }
+
+    @Override
+    public String toString() {
+        return "Mean (" + N + " values): " + String.format("%7.5f", mean());
+    }
+}
